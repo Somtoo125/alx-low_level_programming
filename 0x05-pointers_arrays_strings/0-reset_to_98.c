a26998cd721d1bde73a8f02881d11685da531462
Somtoo125 <okekemercy10@gmail.com>
2023-06-27 15:09:11 -0700

    satisfied

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - function takes a pointer  and updates to 98
- * @n! - the value to be updated to 98
+ * @n: - the value to be updated to 98
  * Return: n
  */
 void reset_to_98(int *n)
